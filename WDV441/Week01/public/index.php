471f90105ca4542f64c85624d08d8e5c615bf69d
TristonNearmyer <49414559+TristonNearmyer@users.noreply.github.com>
2021-01-28 01:14:57 -0600

    Add files via upload

--- WDV441/Week01/public/index.php
+++ WDV441/Week01/public/index.php
@@ -1,10 +1,66 @@
 <!doctype html>
+
+<?php
+
+	$userNameArray = array("Triston","Tyler","Rai","Zach","Kay","Ian","Nathan","Jenna","Louie","Maddison");
+
+	$randomNumber = rand(0,20);
+
+	function displayUserName(){
+		global $randomNumber; 
+		
+		global $userNameArray;
+		
+		$indexNumber = $randomNumber;
+		
+		
+		if($indexNumber <= 9){
+			echo("<p> Hello " . $userNameArray[$indexNumber] . "</p><br><img src='bear.gif' alt='bear waving hello' width='450px' height='490px'>");
+		}else{
+			echo("<p> Name List: </br></p>");
+				foreach($userNameArray as $value){
+				echo("<p>" . $value . "</p><br>");
+			}
+		}
+	}
+
+	displayUserName();
+
+?>
+
+
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Week 1 Assignment</title>
+	
+	<style>
+		
+		body {
+			background-image: linear-gradient(darkblue,blue);
+			background-repeat: no-repeat;
+			background-size: cover;
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+		}
+	
+		p {
+			font-size: 70px;
+			color: white;
+			width: 50%;
+			display: flex;
+			justify-content: center;
+		}
+	
+	</style>
+	
+	
 </head>
 
 <body>
+	
+	
+	
 </body>
 </html>
