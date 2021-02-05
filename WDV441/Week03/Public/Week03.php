dd18cdc6c0bf274ae19475ab7b644ac4447dcd99
triston nearmyer <tcnearmyer@dmacc.edu>
2021-02-04 20:11:03 -0600

    week 3 hw

---
+++ WDV441/Week03/Public/Week03.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<?php 
+
+	$userID = "";
+	$firstName = "";
+	$lastName = "";
+	$email = "";
+	$phoneNumber = "";
+	$password = "";
+	$userDataArray = [$userID, $firstName, $lastName, $email, $phoneNumber, $password];
+
+	function loadData($tableName, $keyField, $keyValue){
+		
+	}
+
+	function validateData($dataArray){
+		
+	}
+
+	function saveData($dataArray){
+		
+	}
+
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>WDV441 Week 3</title>
+</head>
+
+<body>
+</body>
+</html>
